a2f20977ff4e5bf2bd7b0001645b7055579fb15f
Anjalireddy888 <112315720+Anjalireddy888@users.noreply.github.com>
2023-04-29 10:18:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+//caraousel// media//image slider 
+
+
+let onSlide = false;
+
+window.addEventListener("load", () => {
+   autoSlide();
+
+   const dots = document.querySelectorAll(".carousel_dot");
+   for (let i = 0; i < dots.length; i++) {
+      dots[i].addEventListener("click", () => slide(i));
+   }
+
+   const buttonPrev = document.querySelector(".carousel_button__prev");
+   const buttonNext = document.querySelector(".carousel_button__next");
+   buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
+   buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
+})
+
+function autoSlide() {
+   setInterval(() => {
+      slide(getItemActiveIndex() + 1);
+   }, 3000); 
+}
+
+function slide(toIndex) {
+   if (onSlide)
+      return;
+   onSlide = true;
+
+   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+   const itemActive = document.querySelector(".carousel_item__active");
+   const itemActiveIndex = itemsArray.indexOf(itemActive);
+   let newItemActive = null;
+
+   if (toIndex > itemActiveIndex) {
+      
+      if (toIndex >= itemsArray.length) {
+         toIndex = 0;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      
+      newItemActive.classList.add("carousel_item__pos_next");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__next");
+         itemActive.classList.add("carousel_item__next");
+      }, 20);
+   } else {
+    
+      if (toIndex < 0) {
+         toIndex = itemsArray.length - 1;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      // start transition
+      newItemActive.classList.add("carousel_item__pos_prev");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__prev");
+         itemActive.classList.add("carousel_item__prev");
+      }, 20);
+   }
+
+   newItemActive.addEventListener("transitionend", () => {
+      itemActive.className = "carousel_item";
+      newItemActive.className = "carousel_item carousel_item__active";
+      onSlide = false;
+   }, {
+      once: true
+   });
+
+   slideIndicator(toIndex);
+}
+
+function getItemActiveIndex() {
+   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+   const itemActive = document.querySelector(".carousel_item__active");
+   const itemActiveIndex = itemsArray.indexOf(itemActive);
+   return itemActiveIndex;
+}
+
+function slideIndicator(toIndex) {
+   const dots = document.querySelectorAll(".carousel_dot");
+   const dotActive = document.querySelector(".carousel_dot__active");
+   const newDotActive = dots[toIndex];
+
+   dotActive.classList.remove("carousel_dot__active");
+   newDotActive.classList.add("carousel_dot__active");
+}
+
+
+
+ScrollReveal({
+    reset: true,
+    distance:'150px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading,.people,.members,.thinktank,.media,.knowus  ', { orgin:'top'});
+ScrollReveal().reveal('events.container' , { orgin:'bottom'});
+ScrollReveal().reveal('.home-content h1,', { orgin:'left'});
+ScrollReveal().reveal('.home-content p, ', { orgin:'right'});
